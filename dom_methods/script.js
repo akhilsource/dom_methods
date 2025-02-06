80c918a289c4040b61e657a679dc9a0d9eb191cb
venkateshulli45 <deepcodervenky@gmail.com>
2025-02-06 17:53:12 +0530

    added containers

--- dom_methods/script.js
+++ dom_methods/script.js
@@ -1,13 +1,1 @@
-*{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
 
-body{
-    font-family: Arial, sans-serif;
-    bg-color: #FEF9E1
-}
-
---main-color: #3498db;
---secondary-color: #2ecc71;
