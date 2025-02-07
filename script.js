7785f6c7b2622b674294b243d9d7feb6a5d7652f
akhilsource <saiakhil14884@gmail.com>
2025-02-07 11:53:36 +0530

    hello

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-let names = ["Akhil", "venky", "Manjunath", "ramu", "sandya", "kerrthi", "manasa",
-    "abhi", "rakesh", "giridhar", "yokshitha"
+let names = ["Akhil", "Venky", "Manjunath", "Ramu", "Sandya", "Kerrthi", "Manasa",
+    "Abhi", "Rakesh", "Giridhar", "Yokshitha"
 ];
 let sal = [];
+for (let i = 0; i < names.length; i++) {
+    sal.push(Math.floor(Math.random() * 100000) + 20000);
+}
+console.log(sal);
 
-window.onload = function() {
-    for (let i = 0; i < names.length; i++) {
-        sal.push(Math.floor(Math.random() * 100000) + 20000);
-    }
-    console.log(sal);
-};
+
 let index = 0;
 
 function add() {
-    index += 1;
+    if (index >= names.length) {
+        alert("No more names left to add!");
+        return;
+    }
+
     let table = document.querySelector(".table");
-    let row = table.querySelector(".row");
-    let new_row = row.cloneNode(true);
+
+    // Create a new row instead of cloning
+    let new_row = document.createElement("tr");
+
     let td1 = document.createElement("td");
     let td2 = document.createElement("td");
-    td1.innerHTML = `<p>${names[index]}</p>`;
-    td1.innerHTML = `<p>${sal[index]}</p>`;
 
+    td1.innerHTML = names[index];
+    td2.innerHTML = sal[index];
+    new_row.appendChild(td1);
+    new_row.appendChild(td2);
+    table.appendChild(new_row);
+    index++; // Move to the next index
 }
